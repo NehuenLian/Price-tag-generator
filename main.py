0a01c8a5fec310d4cda2a51201ecab0f9f648a90
Nehuen Lian <nehuenn.lian@gmail.com>
2025-09-15 00:31:01 -0300

    minor changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tool.barcode_generator import generate_barcode
 from tool.generate_pricetag import create_pricetag
 
 def main():
-    generate_barcode('7790895000997', 'ean13')
+    generate_barcode('7790895000997', 'ean13') ## example data
     create_pricetag("COCA COLA SABOR ORIGINAL 2.25L", 
                     3852.81,
                     2892.56,
